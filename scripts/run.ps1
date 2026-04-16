fbbe2b2c10356c0d0b5a84d50350ec0c4664e136
Lukáš Lechman <xlechman@stuba.sk>
2026-04-16 21:31:55 +0200

    mongodb

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -11,13 +11,28 @@ $ProjectRoot = "${PSScriptRoot}/.."
 $env:EQUIPTRACK_API_ENVIRONMENT="Development"
 $env:EQUIPTRACK_API_PORT="8080"
 
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
+
 switch ($command) {
-    "start" {
-        go run ${ProjectRoot}/cmd/equiptrack-api-service
-    }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
     }
+    "start" {
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/equiptrack-api-service
+        }finally {
+            mongo down
+    }
+    "mongo" {
+        mongo up
+    }
+
     default {
         throw "Unknown command: $command"
     }
